81c8620f07c77713823d95df0b74d73b08c51c79
aleksandrefanov <mr.eeers@mail.ru>
2020-11-25 02:55:47 +0500

    End

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <main>
+    <TaskInput @onAddTask="addTask"></TaskInput>
+    <ul class="my-list task-list">
+      <li v-for="item in taskList" :key="item.id">
+        <TaskCart @onRemove="removeTask(item.id)"  @onDone="setDoneTask(item.id)" :model="item"></TaskCart>
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TaskCart from "./components/TaskCart.vue";
+import TaskInput from "./components/TaskInput.vue";
+import {ref} from 'vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TaskCart,
+    TaskInput
+  },
+  setup() {
+    const taskList = ref([{id: 0, title: 'Create task', description: 'description test', status: false}])
+
+    const addTask = ({title, description}) => {
+      taskList.value = [...taskList.value, {id: taskList.value[taskList.value - 1].id + 1, title, description, status: false}]
+    }
+
+    const setDoneTask = (id) => {
+      taskList.value = taskList.value.map(x => {
+        if(x.id === id)
+          x.status = true
+        return x;
+      })
+    }
+    const removeTask = (id) => {
+      taskList.value = taskList.value.filter(x => x.id !== id);
+    }
+
+    return {
+      taskList,
+      addTask,
+      removeTask,
+      setDoneTask
+    }
   }
 }
 </script>
+
+<style scoped>
+  .task-list {
+    list-style: none;
+  }
+</style>
---
+++ src/components/TaskCart.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="task-cart my-style">
+    <div>
+      <h4>{{model.title}}</h4>
+      <p>{{model.description}}</p>
+    </div>
+    <div>
+      <button @click="emitOnDone" v-if="!model.status">✅</button>
+      <button @click="emitOnRemove" v-else>❌</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['onDone', 'onRemove'],
+  props: {
+    model: {
+      required: true,
+      default: {
+        id: 0,
+        title: "Create task",
+        description: "Task description",
+        status: false
+      }
+    }
+  },
+
+  setup(props, {emit}) {
+    const emitOnDone = () => {
+      emit('onDone')
+    }
+    const emitOnRemove = () => {
+      emit('onRemove')
+    }
+
+    return {
+      emitOnDone,
+      emitOnRemove
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .task-cart {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
